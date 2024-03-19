58787e43e96a6789b4c3fbbe8e5397c196dd0ef3
Liia Rimpiläinen <liia@oru.fi>
2024-03-19 13:14:29 +0200

    Edited rofi config

--- bin/rofi_do.sh
+++ bin/rofi_do.sh
@@ -8,7 +8,7 @@ read -r cmd1 cmd2 rest
 
 case "$cmd1" in
 	"add")
-		alias | which -ai $cmd2 > /dev/null 2>&1 && \
+        which -a $cmd2 > /dev/null 2>&1 && \
 		echo "$cmd2" >> $FILE && \
 		sort $FILE -o $FILE
 		cat $FILE | $DMENU | rofi_do.sh
